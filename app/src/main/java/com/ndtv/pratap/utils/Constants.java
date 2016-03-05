57ec2debe82e1ee74a2c24359e8ee3b02c5738b7
fenghuigithub2 <fenghui@lawn-128-61-27-193.lawn.gatech.edu>
2016-03-05 14:31:38 -0500

    test

---
+++ app/src/main/java/com/ndtv/pratap/utils/Constants.java
@@ -0,0 +1,32 @@
+package com.ndtv.pratap.utils;
+
+/**
+ * Created by pratap.kesaboyina on 24-12-2014.
+ */
+public class Constants {
+
+    public static final String TOPSTORIES[] =
+            {"http://feeds.feedburner.com/NdtvNews-TopStories?format=xml",
+             "http://feeds.feedburner.com/NDTV-LatestNews?format=xml",
+             "http://feeds.feedburner.com/NDTV-Trending?format=xml",
+             "http://feeds.feedburner.com/NDTV-Business?format=xml",
+             "http://feeds.feedburner.com/NDTV-Cricket?format=xml",
+             "http://feeds.feedburner.com/NDTV-Ent?format=xml",
+            };
+
+          //  TOPSTORIES ="http://feeds.feedburner.com/NdtvNews-TopStories?format=xml";
+
+    // Latest Stories : http://feeds.feedburner.com/NDTV-LatestNews?format=xml
+
+    // Trending Stories : http://feeds.feedburner.com/NDTV-Trending?format=xml
+
+    //For movies:  http://feeds.feedburner.com/NDTV-Ent
+
+    // For Top Stories: http://feeds.feedburner.com/NdtvNews-TopStories
+
+    // For Business: http://feeds.feedburner.com/NDTV-Business
+
+    // For World News: http://feeds.feedburner.com/ndtv/TqgX
+
+    // For Cricket : http://feeds.feedburner.com/NDTV-Cricket?format=xml
+}
